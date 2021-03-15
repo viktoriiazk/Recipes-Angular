f4cc49729985190169987f8ee6f4ab360065be16
Viktoriia Zakorchemna <viktoriia.zakorchemna@gmail.com>
2021-03-15 13:20:48 +0200

    feat: add shoping list edit section

--- recipe-project/src/app/shoping-list/shoping-list.component.ts
+++ recipe-project/src/app/shoping-list/shoping-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Ingredient } from '../shared/ingredient.model';
 @Component({
   selector: 'app-shoping-list',
   templateUrl: './shoping-list.component.html',
   styleUrls: ['./shoping-list.component.css']
 })
 export class ShopingListComponent implements OnInit {
+
+Ingridients:Ingredient[] = [
+  new Ingredient('Apple', 5),
+  new Ingredient('Lemons', 2),
+];
 
   constructor() { }
 
